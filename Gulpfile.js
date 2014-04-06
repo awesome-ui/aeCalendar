b7ddd4083318300d312ba36f0ab45ae6353da4ac
Michael F <tehfreak@awesome39.com>
2014-04-06 21:27:29 +0300

    Fixed and improved building

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,14 @@
 var gulp= require('gulp')
 var gulpNgmin= require('gulp-ngmin')
 var gulpUglify= require('gulp-uglify')
+var gulpRename= require('gulp-rename')
 
 gulp.task('js', function () {
     return gulp.src(['src/aeCalendar/calendar.js'])
+        .pipe(gulp.dest('build'))
         .pipe(gulpNgmin())
         .pipe(gulpUglify())
+        .pipe(gulpRename({suffix: '.min'}))
         .pipe(gulp.dest('build'))
     ;
 })
